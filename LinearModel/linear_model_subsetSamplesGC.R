5344f88d1b83a52ef14a11d9f8c08db69e804aab
kwoyshner <45450805+kwoyshner@users.noreply.github.com>
2026-04-10 09:04:55 -0400

    upload lmer scripts

---
+++ LinearModel/linear_model_subsetSamplesGC.R
@@ -0,0 +1,73 @@
+library(spatstat.explore)
+library(Seurat)
+library(lme4)
+
+meta <- read.csv("/mnt/morbo/Data/Users/kwoyshner/cerebellum/data/clinical_meta_merged.csv", row.names = 1)
+
+meta$patient_replicate_CT <- paste0(meta$patient, meta$replicate, '_', meta$Cell_Type)
+
+exp_data <- load('/mnt/morbo/Data/Users/kwoyshner/cerebellum/data/human_sobj_transformed_projected.rda')
+spatial_matrix <- as.matrix(sobj@assays[["Spatial"]]@counts) # genes by spots
+spatial_matrix <- spatial_matrix[,apply(spatial_matrix,2,max)>0] # Remove spots with no signal
+spatial_matrix <- spatial_matrix[apply(spatial_matrix,1,max)>0,] # Remove genes with no signal
+
+log_spatial_matrix <- log1p(spatial_matrix)
+log_spatial_matrix <- t(log_spatial_matrix) ## CHECK THIS IS SAMPLES BY GENES NOW
+
+meta <- meta[rownames(log_spatial_matrix),] # make sure these align
+
+data <- merge(
+    x = log_spatial_matrix,
+    y = meta[,c('patient', 'Age.at.Death..weeks.', 'Gestational.Age..Weeks.', 'Cell_Type')],
+    by = "row.names" # should be samples
+    )
+
+data$Preterm <- ifelse(data$Gestational.Age..Weeks. < 38, 1, 0)
+data_ptsub <- data[data$patient != "outlier", ]  # Remove outlier patient (the 3 year old)
+data_ptsub <- data_ptsub[data_ptsub$Age.at.Death..weeks. > 45, ]
+data_ptsub$patient <- as.factor(data_ptsub$patient)
+
+#######################################################
+#####################     SETUP   #####################
+#######################################################
+
+#### MODEL DESIGN :     model <- lmer(gene_expression_single ~ (1 | patient) + Gestational.Age..Weeks., data = data_sub)
+variables <- c("Intercept", "Gestational.Age..Weeks.")
+beta_estimates_colnames <- c(variables, paste0("t_val_", variables), paste0("F_val_", variables[-1]))
+savename <- '/mnt/morbo/Data/Users/kwoyshner/cerebellum/results/linear_model/version15/coefficients'
+
+
+#######################################################
+#####################      GC     #####################
+#######################################################
+beta_estimates <- data.frame(matrix(ncol = length(beta_estimates_colnames), nrow = 0))
+colnames(beta_estimates) <- beta_estimates_colnames
+data_sub <- data_ptsub[data_ptsub['Cell_Type'] == 'Granular',] # SUBSET TO GCs
+
+for (gene in colnames(log_spatial_matrix)) { # Loop through each gene
+    gene_expression_single <- data_sub[, gene] # Extracting gene expression for a single gene
+    model <- lmer(gene_expression_single ~ (1 | patient) + Gestational.Age..Weeks., data = data_sub) # Define the model
+    coefs <- fixef(model)     # Extract the fixed effects coefficients
+    beta_estimates[gene, variables] <- coefs    # Add the coefficients to the beta_estimates data frame
+
+    # Extract the t values
+    tryCatch(
+        expr = {
+            # Extract the t values
+            t_vals <- summary(model)[["coefficients"]][,"t value"]
+            names(t_vals) <- paste0('t_val_', names(t_vals))
+            beta_estimates[gene, paste0("t_val_", variables)] <- t_vals
+
+            f_vals <- unlist(anova(model)["F value"])
+            names(f_vals) <- paste0('F_val_', rownames(anova(model)["F value"]))
+            beta_estimates[gene, paste0("F_val_", variables[-1])] <- f_vals
+        },
+        error=function(error_message) {
+            message(paste0("error in ", gene))
+            beta_estimates[gene, paste0("t_val_", variables)] <- c(0,0)
+            beta_estimates[gene, paste0("F_val_", variables[-1])] <- c(0)
+        }
+
+    )
+}
+write.csv(beta_estimates, paste0(savename, '_Granular.csv'))
---
+++ LinearModel/plot_heatmap_sampleAveraged_sortGA.R
@@ -0,0 +1,306 @@
+library(spatstat.explore)
+library(Seurat)
+library(lme4)
+library(pheatmap)
+library(CoGAPS)
+library(dplyr)
+
+#######################################################
+###############  PREP EXPRESSION DATA  ################
+#######################################################
+
+load("/mnt/morbo/Data/Users/kwoyshner/cerebellum/data/human_sobj_transformed_projected.rda")
+sobj_sub = sobj
+print(dim(sobj_sub))
+
+spatial_matrix <- as.matrix(sobj_sub@assays[["Spatial"]]@counts)
+log_spatial_matrix <- log1p(spatial_matrix)
+log_spatial_matrix <- t(log_spatial_matrix)
+
+meta <- read.csv("/mnt/morbo/Data/Users/kwoyshner/cerebellum/data/clinical_meta_merged.csv", row.names = 1)
+meta <- meta[rownames(log_spatial_matrix),] # make sure these align
+
+data <- merge(
+    x = log_spatial_matrix,
+    y = meta[,c('patient', 'Gestational.Age..Weeks.', 'Age.at.Death..weeks.', 'AGEDAYSTOTAL', 'Cell_Type')],
+    by = "row.names" # should be samples
+    )
+
+rownames(data) <- data$Row.names
+data$Preterm <- ifelse(data$Gestational.Age..Weeks. < 38, 1, 0) # define preterm as < 38 weeks
+data_ptsub <- data[data$patient != "outlier", ]  # Remove outlier patient (the 3 year old)
+data_ptsub$patient <- as.factor(data_ptsub$patient) # make patient a factor
+data_ptsub <- data_ptsub[data_ptsub$Age.at.Death..weeks. > 45, ] # subset to PMA > 45 (drop low outliers)
+
+n_genes <- 20 # ngenes to plot
+lmer_outputs <- '/mnt/morbo/Data/Users/kwoyshner/cerebellum/results/linear_model/version15/coefficients_'
+
+#sort_col <- 'Gestational.Age..Weeks.'
+#annot_name <- 'Gestational Age (weeks)'
+
+savename <- "/mnt/morbo/Data/Users/kwoyshner/cerebellum/results/linear_model/version15/plot_heatmaps/heatmap_zscore_ptAverage_GAsort_" #coef vs tval
+
+######
+# Notes model is:     model <- lmer(gene_expression_single ~ (1 | patient) + Gestational.Age..Weeks., data = data_sub)
+
+
+#######################################################
+###############        PCs         ####################
+#######################################################
+
+lm_genes <- read.csv(paste0(lmer_outputs, 'Purkinje.csv'), row.names = 1) # input PC file
+lm_genes <- lm_genes[complete.cases(lm_genes), ] # remove NAs
+data_sub <- data_ptsub[data_ptsub['Cell_Type'] == 'Purkinje',]  # subset to PCs
+
+df_summary <- data_sub %>%
+  group_by(patient) %>%
+  summarise(across(where(is.numeric), mean, na.rm = TRUE),  # Average numeric columns
+            across(where(is.character), first))             # Take the first value of non-numeric columns
+
+data_sub_grouped <- as.data.frame(df_summary) # group by patient
+rownames(data_sub_grouped) <- data_sub_grouped$patient
+
+######################################################
+##################    Gestational.Age..Weeks.   #####################
+coef_col <- 'Gestational.Age..Weeks.'
+tval_col <- paste0('t_val_',coef_col)
+
+####################### COEFS ########################
+sorted_coef <- lm_genes[rev(order(lm_genes[[coef_col]])),]
+
+# check the 20 top and bottom genes are pos and neg respectively
+if ((min(head(sorted_coef, n_genes)[[coef_col]]) > 0) & (max(tail(sorted_coef, n_genes)[[coef_col]]) < 0)) {
+    print("All positive and all negative genes selected")
+} else {
+   print("DONT USE THIS")
+}
+
+coef_sigs <- rbind(head(sorted_coef, n_genes),tail(sorted_coef, n_genes))
+sample_info <- data_sub_grouped[order(data_sub_grouped$Gestational.Age..Weeks.,data_sub_grouped$patient),]
+
+gene_info <- data.frame(
+    Direction = c(rep("Positive",n_genes), rep("Negative",n_genes)),
+    Amplitude = abs(coef_sigs[[coef_col]]),
+    row.names = rownames(coef_sigs) 
+    )
+
+annotation_col <- data.frame(
+    Gestational_Age = data_sub_grouped$Gestational.Age..Weeks.,
+    PT = ifelse(data_sub_grouped$Gestational.Age..Weeks. < 38, 'Preterm', 'Term'),
+    patient = factor(data_sub_grouped$patient),
+    row.names = rownames(data_sub_grouped)
+)
+
+data_zScore <- scale(sample_info[,rownames(gene_info)], center = TRUE, scale = TRUE) # checked colMeans ~ 0 and col std aka apply(PC,2,sd) = 1
+
+# Calculate the 5th and 95th percentiles
+vmin <- quantile(t(data_zScore), 0.05)
+vmax <- quantile(t(data_zScore), 0.95)
+my_colors <- colorRampPalette(c("blue", "white", "red"))(100)
+
+max_abs_val <- max(abs(c(vmin, vmax)))
+breaks <- seq(-max_abs_val, max_abs_val, length.out = 101)
+
+pdf(paste0(savename, coef_col,  "_PC.pdf"))
+
+p <- pheatmap(t(data_zScore),
+    annotation_col = annotation_col[rownames(data_zScore),,drop=FALSE],
+    annotation_row = gene_info,
+    cluster_rows=FALSE,
+    cluster_cols=FALSE,
+    show_colnames = FALSE,
+    gaps_row = c(sum(gene_info$Direction == 'Positive')),
+    gaps_col = c(sum(annotation_col$Gestational_Age < 38)),
+    color = my_colors,      
+    breaks = breaks
+    )
+p
+
+dev.off()
+
+
+####################### T VALS ########################
+sorted_tval <- lm_genes[rev(order(lm_genes[[tval_col]])),]
+
+# check the 20 top and bottom genes are pos and neg respectively
+if ((min(head(sorted_tval, n_genes)[[tval_col]]) > 0) & (max(tail(sorted_tval, n_genes)[[tval_col]]) < 0)) {
+    print("All positive and all negative genes selected")
+} else {
+   print("DONT USE THIS")
+}
+
+tval_sigs <- rbind(head(sorted_tval, n_genes),tail(sorted_tval, n_genes))
+
+sample_info <- data_sub_grouped[order(data_sub_grouped$Gestational.Age..Weeks., data_sub_grouped$patient),]
+
+gene_info <- data.frame(
+    Direction = c(rep("Positive",n_genes), rep("Negative",n_genes)),
+    Amplitude = abs(tval_sigs[[tval_col]]),
+    row.names = rownames(tval_sigs) 
+    )
+
+annotation_col <- data.frame(
+    Gestational_Age = data_sub_grouped$Gestational.Age..Weeks.,
+    PT = ifelse(data_sub_grouped$Gestational.Age..Weeks. < 38, 'Preterm', 'Term'),
+    patient = factor(data_sub_grouped$patient),
+    row.names = rownames(data_sub_grouped)
+)
+
+data_zScore <- scale(sample_info[,rownames(gene_info)], center = TRUE, scale = TRUE) # checked colMeans ~ 0 and col std aka apply(PC,2,sd) = 1
+
+# Calculate the 5th and 95th percentiles
+vmin <- quantile(t(data_zScore), 0.05)
+vmax <- quantile(t(data_zScore), 0.95)
+breaks <- seq(vmin, vmax, length.out = 101)
+my_colors <- colorRampPalette(c("blue", "white", "red"))(100)
+max_abs_val <- max(abs(c(vmin, vmax)))
+breaks <- seq(-max_abs_val, max_abs_val, length.out = 101)
+
+pdf(paste0(savename, tval_col,  "_PC.pdf"))
+
+p <- pheatmap(t(data_zScore),
+    annotation_col = annotation_col[rownames(data_zScore),,drop=FALSE],
+    annotation_row = gene_info,
+    cluster_rows=FALSE,
+    cluster_cols=FALSE,
+    show_colnames = FALSE,
+    gaps_row = c(sum(gene_info$Direction == 'Positive')),
+    gaps_col = c(sum(annotation_col$Gestational_Age < 38)),
+    color = my_colors,     
+    breaks = breaks
+    )
+p
+dev.off()
+
+
+
+#######################################################
+###############        GCs         ####################
+#######################################################
+
+lm_genes <- read.csv(paste0(lmer_outputs, 'Granular.csv'), row.names = 1) # input PC file
+lm_genes <- lm_genes[complete.cases(lm_genes), ] # remove NAs
+data_sub <- data_ptsub[data_ptsub['Cell_Type'] == 'Granular',]  # subset to PCs
+
+df_summary <- data_sub %>%
+  group_by(patient) %>%
+  summarise(across(where(is.numeric), mean, na.rm = TRUE),  # Average numeric columns
+            across(where(is.character), first))             # Take the first value of non-numeric columns
+
+data_sub_grouped <- as.data.frame(df_summary) # group by patient
+rownames(data_sub_grouped) <- data_sub_grouped$patient
+
+######################################################
+################## Gestational.Age..Weeks. ######################
+
+coef_col <- 'Gestational.Age..Weeks.'
+tval_col <- paste0('t_val_',coef_col)
+
+####################### COEFS ########################
+sorted_coef <- lm_genes[rev(order(lm_genes[[coef_col]])),]
+
+# check all pos and all neg
+if ((min(head(sorted_coef, n_genes)[[coef_col]]) > 0) & (max(tail(sorted_coef, n_genes)[[coef_col]]) < 0)) {
+    print("All positive and all negative genes selected")
+} else {
+   print("DONT USE THIS")
+}
+
+coef_sigs <- rbind(head(sorted_coef, n_genes),tail(sorted_coef, n_genes))
+sample_info <- data_sub_grouped[order(data_sub_grouped$Gestational.Age..Weeks.,data_sub_grouped$patient),]
+
+gene_info <- data.frame(
+    Direction = c(rep("Positive",n_genes), rep("Negative",n_genes)),
+    Amplitude = abs(coef_sigs[[coef_col]]),
+    row.names = rownames(coef_sigs) 
+    )
+
+annotation_col <- data.frame(
+    Gestational_Age = data_sub_grouped$Gestational.Age..Weeks.,
+    PT = ifelse(data_sub_grouped$Gestational.Age..Weeks. < 38, 'Preterm', 'Term'),
+    patient = factor(data_sub_grouped$patient),
+    row.names = rownames(data_sub_grouped)
+)
+
+data_zScore <- scale(sample_info[,rownames(gene_info)], center = TRUE, scale = TRUE) # checked colMeans ~ 0 and col std aka apply(PC,2,sd) = 1
+
+# Calculate the 5th and 95th percentiles
+vmin <- quantile(t(data_zScore), 0.05)
+vmax <- quantile(t(data_zScore), 0.95)
+my_colors <- colorRampPalette(c("blue", "white", "red"))(100)
+
+max_abs_val <- max(abs(c(vmin, vmax)))
+breaks <- seq(-max_abs_val, max_abs_val, length.out = 101)
+
+pdf(paste0(savename, coef_col,  "_GC.pdf"))
+
+p <- pheatmap(t(data_zScore),
+    annotation_col = annotation_col[rownames(data_zScore),,drop=FALSE],
+    annotation_row = gene_info,
+    cluster_rows=FALSE,
+    cluster_cols=FALSE,
+    show_colnames = FALSE,
+    gaps_row = c(sum(gene_info$Direction == 'Positive')),
+    gaps_col = c(sum(annotation_col$Gestational_Age < 38)),
+    color = my_colors,   
+    breaks = breaks
+    )
+p
+
+dev.off()
+
+
+####################### T VALS ########################
+sorted_tval <- lm_genes[rev(order(lm_genes[[tval_col]])),]
+
+# check all pos and all neg
+if ((min(head(sorted_tval, n_genes)[[tval_col]]) > 0) & (max(tail(sorted_tval, n_genes)[[tval_col]]) < 0)) {
+    print("All positive and all negative genes selected")
+} else {
+   print("DONT USE THIS")
+}
+
+tval_sigs <- rbind(head(sorted_tval, n_genes),tail(sorted_tval, n_genes))
+
+sample_info <- data_sub_grouped[order(data_sub_grouped$Gestational.Age..Weeks., data_sub_grouped$patient),]
+
+gene_info <- data.frame(
+    Direction = c(rep("Positive",n_genes), rep("Negative",n_genes)),
+    Amplitude = abs(tval_sigs[[tval_col]]),
+    row.names = rownames(tval_sigs) 
+    )
+
+annotation_col <- data.frame(
+    Gestational_Age = data_sub_grouped$Gestational.Age..Weeks.,
+    PT = ifelse(data_sub_grouped$Gestational.Age..Weeks. < 38, 'Preterm', 'Term'),
+    patient = factor(data_sub_grouped$patient),
+    row.names = rownames(data_sub_grouped)
+)
+
+data_zScore <- scale(sample_info[,rownames(gene_info)], center = TRUE, scale = TRUE) # checked colMeans ~ 0 and col std aka apply(PC,2,sd) = 1
+
+# Calculate the 5th and 95th percentiles
+vmin <- quantile(t(data_zScore), 0.05)
+vmax <- quantile(t(data_zScore), 0.95)
+breaks <- seq(vmin, vmax, length.out = 101)
+my_colors <- colorRampPalette(c("blue", "white", "red"))(100)
+max_abs_val <- max(abs(c(vmin, vmax)))
+breaks <- seq(-max_abs_val, max_abs_val, length.out = 101)
+
+pdf(paste0(savename, tval_col,  "_GC.pdf"))
+
+p <- pheatmap(t(data_zScore),
+    annotation_col = annotation_col[rownames(data_zScore),,drop=FALSE],
+    annotation_row = gene_info,
+    cluster_rows=FALSE,
+    cluster_cols=FALSE,
+    show_colnames = FALSE,
+    gaps_row = c(sum(gene_info$Direction == 'Positive')),
+    gaps_col = c(sum(annotation_col$Gestational_Age < 38)),
+    color = my_colors,  
+    breaks = breaks
+    )
+p
+dev.off()
+
+
